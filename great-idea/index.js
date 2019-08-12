d3edf73e6d4917525fe75565ac81ff4939c1cb90
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-08-12 07:51:46 -0700

    added cool animations to the flip-card on Services page

---
+++ great-idea/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// CHAT -------CHAT -------CHAT -------CHAT -------
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
