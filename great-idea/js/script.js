cd452948f972b56cafd7c0bcb424955b3c7ca835
kevin lautermilch <kevin.laut@outlook.com>
2020-01-16 19:57:56 -0500

    :)

---
+++ great-idea/js/script.js
@@ -0,0 +1,8 @@
+var form = document.getElementById("contact-form");
+
+
+function formButton() {
+    x = document.getElementsByClassName("hideme")[0];
+    x.classList.remove("hideme");
+    window.scrollTo(0,document.body.scrollHeight);
+}
